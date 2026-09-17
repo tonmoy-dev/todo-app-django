d69080d285bbaec7aa085d16615a8ad1a961a991
tonmoy-dev <rxdevs100@gmail.com>
2026-09-17 15:23:24 +0600

    add database and configure the admin site

--- todoApp/todoApp/settings.py
+++ todoApp/todoApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos.apps.TodosConfig',
 ]
 
 MIDDLEWARE = [
@@ -73,9 +74,13 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo_db",
+        "USER": "upay",
+        "PASSWORD": "roxen100",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
--- todoApp/todos/models.py
+++ todoApp/todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+  title = models.CharField(max_length=100)
+  details = models.CharField(max_length=255)
+  isCompleted = models.BooleanField(default=False)
+
+
